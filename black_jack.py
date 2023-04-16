ced26ef5438280fd364500b7e649875c469b7da5
S3762990 <118754944+S3762990@users.noreply.github.com>
2023-04-16 16:43:00 -0400

    Update black_jack.py

--- black_jack.py
+++ black_jack.py
@@ -2,17 +2,17 @@ import requests
 import json
 import pprint
 
-dc = input("How many decks would you like? ")
-url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=" + str(dc)
+dc= input("How many decks would you like? ")
+url= "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=" + str(dc)
 
-payload={}
-headers = {'Accept': 'application/json'}
+payload= {}
+headers= {'Accept': 'application/json'}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+response= requests.request("GET", url, headers=headers, data=payload)
 
-deckJson = response.json()
+deckJson= response.json()
 
-id = deckJson['deck_id']
+id= deckJson['deck_id']
 print('Your deck ID is ' + str(id))
 
 
